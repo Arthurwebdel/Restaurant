32fd55d9e5c26374a02a22b87fbaf1bb132df052
Arthurwebdel <113822476+Arthurwebdel@users.noreply.github.com>
2022-12-26 15:50:20 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,101 @@
+$(function(){
+    $('.slider-inner').slick({
+        arrows:false,
+        dots:true
+    });
+    $('.bar').on('click', function () {
+        $('.menu,.header-right').slideToggle();
+    });
+    var mixer = mixitup('.dishes-images', {
+        load: {
+            filter: '.dishes_1'
+        }
+    });
+});
+
+   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+  
